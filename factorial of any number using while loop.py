762f6ef80526cdb193fc0eabedbdc76a74469294
Sumair Izhaan <63188497+izhaan0001@users.noreply.github.com>
2024-12-05 22:10:41 +0530

    Add files via upload

---
+++ factorial of any number using while loop.py
@@ -0,0 +1,12 @@
+#basic factorial programs by looping
+# How to find factorial of any number
+num=int(input("Enter your number: ")) #Enter the number you want to find factorial of
+
+#We are using while loop
+i=1
+F=1 #try putting F=0 you will find why I did put F=1
+while(i<=num):
+    F=F*i
+    i+=1 #increment value of i by 1
+
+print("Factorial of given number: ",F)
